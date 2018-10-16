da26e82f69bacbb770c85bf9fc88c377fe62b9f5
Wangtao <853113978@qq.com>
2018-10-16 19:28:43 +0800

    加入显示天气信息的功能。.

---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,18 @@
+package com.coolweather.android.gson;
+
+import android.text.style.UpdateAppearance;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperatrue;
+    @SerializedName("cond")
+    public More more;
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
